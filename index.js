f32d7d9b0ab823a4949791edfd13ed17f2deee94
sierraskyewallace <sskwallace@gmail.com>
2021-07-28 13:06:36 -0700

    done

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+
+var multiply = num1 * num2;
+
+///////////////////////////
+
+
+var random = Math.floor(Math.random() * 100) + 1;
+
+
+///////////////////////////
+
+let num3 = 9;
+let num4 = 5;
+var mod = num3 % num4;
+
+///////////////////////////
+
+var max = 20;
